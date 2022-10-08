396666e9ec8ae7c036d30b6395b31353178ab916
Evandro R.  Tiburcio <evandrorti@gmail.com>
2022-10-08 01:19:30 -0300

    Criacao do Parceiros

--- maestro/src/main/java/br/com/integracorp/entities/PartnerEntity.java
+++ maestro/src/main/java/br/com/integracorp/entities/PartnerEntity.java
@@ -8,6 +8,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
