450b48344bad5c277241407a7cd0a6d50186d90a
Evandro R Tiburcio <evandrorti@gmail.com>
2022-11-03 14:54:18 -0300

    ultima versao

--- maestro/src/main/java/br/com/integracorp/resources/PersonResource.java
+++ maestro/src/main/java/br/com/integracorp/resources/PersonResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.integracorp.entities.PersonEntity;
 import br.com.integracorp.servicies.PersonService;
 
+@CrossOrigin(allowedHeaders = "*",origins = "*")
 @RestController
 @RequestMapping
 public class PersonResource {
@@ -25,18 +28,21 @@ public class PersonResource {
 	@Autowired
 	private PersonService personService;
 	
-	@GetMapping(value="/people")
+	@CrossOrigin
+	@GetMapping(value="/people", produces="application/json")
 	public ResponseEntity<List<PersonEntity>> findAll(){
 		List<PersonEntity> list = personService.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
+	@CrossOrigin
 	@GetMapping(value="/person/{id}")
 	public ResponseEntity<PersonEntity> findById(@PathVariable Long id){
 		PersonEntity person = personService.findById(id);
 		return ResponseEntity.ok().body(person);
 	}
 	
+	@CrossOrigin
 	@PostMapping(value="/person")
 	public ResponseEntity<PersonEntity> save(@RequestBody PersonEntity person)
 	{
@@ -49,6 +55,7 @@ public class PersonResource {
 		return ResponseEntity.created(uri).body(person);
 	}
 	
+	@CrossOrigin
 	@PutMapping(value="/person/{id}")
 	public ResponseEntity<PersonEntity> update(@PathVariable Long id, @RequestBody PersonEntity person)
 	{
@@ -56,6 +63,7 @@ public class PersonResource {
 		return ResponseEntity.ok().body(person);
 	}
 	
+	@CrossOrigin
 	@DeleteMapping(value="/person/{id}")
 	public ResponseEntity<Void> delete (@PathVariable Long id)
 	{
